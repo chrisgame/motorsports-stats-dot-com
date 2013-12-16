803d35c2b5b493eb03059d26b761227f248261ba
Chris Game <chris.game@robbiebobbins.com>
2013-12-16 00:25:56 +0000

    added nested routes for f1. only season list is working so far

--- app/router.js
+++ app/router.js
@@ -4,11 +4,19 @@ Router.map(function() {
   this.route('component-test');
   this.route('helper-test');
   this.route('f1-fastest-laps');
-  this.route('f1-seasons');
   this.route('multi-series-line');
   // this.resource('posts', function() {
   //   this.route('new');
   // });
+  this.resource('f1', function(){
+    this.resource('seasons');
+    this.resource('season', {path: 'season/:season_id'}, function(){
+      this.resource('races');
+      this.resource('race', {path: 'race/:race_number'}, function(){
+        this.route('fastest-laps');
+      });
+    });
+  });
 });
 
 export default Router;
--- app/routes/f1_fastest_laps.js
+++ app/routes/f1_fastest_laps.js
@@ -1,7 +1,15 @@
 export default Ember.Route.extend({
-  model: function() {
-    return {
-      name: "fred"
-    };
-  }
+//  setupController: function(controller, model){
+//    this.controllerFor('seasons').set('content', this.get('store').find('season'));
+//    debugger;
+//    this.controllerFor('races').set('content', this.get('store').find('race'));
+//    controller.set('content', model);
+//  },
+
+//  model: function(){
+//    var seasons = this.get('store').find('season');
+//    var races = this.get('store').find('race');
+//    var fastestLaps = this.get('store').find('fastest_lap');
+//    debugger;
+//  }
 });
