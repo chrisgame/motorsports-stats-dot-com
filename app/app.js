c563720271bdace424bf85b95442218099ba2c75
Chris Game <chris.game@robbiebobbins.com>
2014-01-03 21:03:25 +0000

    Fix for data tab in the inspector

--- app/app.js
+++ app/app.js
@@ -19,4 +19,17 @@ Ember.RSVP.configure('onerror', function(error) {
   }
 });
 
+DS.DebugAdapter.reopen({
+  getModelTypes: function() {
+    var self = this;
+    return Ember.keys(requirejs._eak_seen).filter(function(key) {
+      return !!key.match(/^appkit\/models\//) && self.detect(require(key).default);
+    }).map(function(key){
+      var type = require(key).default, typeKey = key.match(/^appkit\/models\/(.*)/)[1];
+      type.toString = function() { return typeKey; };
+      return type;
+    });
+  }
+});
+
 export default App;
