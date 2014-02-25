4ece2977d57def6239f532f8a2d722fe2c66130e
Chris Game <chris.game@robbiebobbins.com>
2014-02-25 09:13:02 +0000

    Upgraded app-kit

--- app/transforms/duration.js
+++
@@ -1,6 +0,0 @@
-export default DS.Transform.extend({
-  deserialize: function(serialized) {
-    var duration = moment.duration(parseFloat(serialized), 'seconds');
-    return serialized ? duration.minutes() + ':' + duration.seconds() + '.' + duration.milliseconds() : null;
-  }
-});
