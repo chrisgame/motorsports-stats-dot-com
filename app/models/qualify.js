ac13227867815f8ed907651461727c69b56cabeb
Chris Game <chris.game@robbiebobbins.com>
2014-02-27 19:21:45 +0000

    made qualifies and practices has one relationships

--- app/models/qualify.js
+++ app/models/qualify.js
@@ -2,5 +2,5 @@ export default DS.Model.extend({
   round: DS.belongsTo('round'),
   best_sectors: DS.hasMany('best_sector', {async:true}),
   speed_traps: DS.hasMany('speed_trap', {async:true}),
-  results: DS.hasMany('result', {async:true})
+  results: DS.hasMany('qualify_result', {async:true})
 });
--- app/models/race.js
+++ app/models/race.js
@@ -2,5 +2,5 @@ export default DS.Model.extend({
   round: DS.belongsTo('round'),
   fastest_laps: DS.hasMany('fastest_lap', {async:true}),
   pit_stops: DS.hasMany('pit_stop', {async:true}),
-  results: DS.hasMany('result', {async:true})
+  results: DS.hasMany('race_result', {async:true})
 });
