670ab04165cd97896170d5656f93623a434a82db
Chris Game <chris.game@robbiebobbins.com>
2014-04-04 21:01:30 +0100

    pass param from url to controller to be reused by nested route

--- app/routes/round.js
+++ app/routes/round.js
@@ -1,5 +1,6 @@
 export default Ember.Route.extend({
   model: function(params){
+    var season = this.controllerFor('season').get('seasonParam');
     var rounds = this.modelFor('season').get('data.rounds');
     return this.get('store').find('round', parseInt(rounds.findBy('name', params.round).id));
   }
