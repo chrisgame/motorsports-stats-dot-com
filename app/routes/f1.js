d7b160e19a4b0d8bc98cc6bab72f587d40270a46
Chris Game <chris.game@robbiebobbins.com>
2014-04-10 19:24:27 +0100

    ordered season list to show most recent first

--- app/routes/f1.js
+++ app/routes/f1.js
@@ -1,5 +1,8 @@
 export default Ember.Route.extend({
   model: function(){
     return this.get('store').find('season');
+  },
+  afterModel: function(model, transition){
+    model = model.reverseObjects();
   }
 });
