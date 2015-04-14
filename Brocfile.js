fd662a33b33c64de73091b4f14e90d2a291b2f9c
The Game <chris.game@robbiebobbins.com>
2015-04-15 00:20:32 +0100

    ember data no longer requires an app.import

--- Brocfile.js
+++ Brocfile.js
@@ -18,7 +18,6 @@ var app = new EmberApp();
 // along with the exports of each module as its value.
 
 app.import('vendor/_amdize.js');
-app.import('vendor/ember-data/ember-data.js');
 app.import('vendor/moment/moment.js', { exports: { moment: ['default'] } });
 app.import('vendor/bootstrap/dist/css/bootstrap.css');
 module.exports = app.toTree();
