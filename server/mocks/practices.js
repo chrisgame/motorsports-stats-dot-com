c4b475973713e6e118848c9d05643c95de4202ee
The Game <chris.game@robbiebobbins.com>
2015-04-22 19:55:31 +0100

    routes that are requested with an id as part of the url eg "sessions/123" should grab the parameter passed in and only return the payload relevant for that id

--- server/mocks/practices.js
+++ server/mocks/practices.js
@@ -1,19 +1,24 @@
 module.exports = function(app) {
   var express = require('express');
   var practiceRouter = express.Router();
-  practiceRouter.get('/practices/*', function(req, res) {
-    res.send(
-      {
-          "practice": {
-              "id": 898, 
-              "practice_session_ids": [
-                  540, 
-                  541, 
-                  542
-              ]
-          }
-      }
-    )
+  practiceRouter.get('/practices/:id', function(req, res) {
+    var result;
+
+    switch(req.params.id){
+      case '898':
+      result = {
+        "practice": {
+          "id": 898,
+          "practice_session_ids": [
+              540,
+              541,
+              542
+          ]
+        }
+      };
+      break;
+    }
+    res.send(result)
   });
   app.use('/', practiceRouter);
-};  
+};
