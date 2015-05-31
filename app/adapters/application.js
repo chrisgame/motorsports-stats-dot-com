69156df5ef2614b788684cab0977cb6618af7e7c
The Game <chris.game@robbiebobbins.com>
2015-05-31 20:52:30 +0100

    added mirage to allow acceptance testing as the express stub is not started for tests anymore - only move the seasons route so far

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.ActiveModelAdapter.reopen({
-  host: 'https://prod-motorsports-stats-api.herokuapp.com/',
-  //host: 'http://localhost:4200',
+  //host: 'https://prod-motorsports-stats-api.herokuapp.com/',
+  host: '',
 });
