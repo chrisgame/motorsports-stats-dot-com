3475f978158319fcf3b721ce39ea9f0717a03187
The Game <chris.game@robbiebobbins.com>
2015-10-11 21:42:56 +0100

    re-initializing

--- app/app.js
+++ app/app.js
@@ -1,14 +1,18 @@
 import Ember from 'ember';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
+import config from './config/environment';
+
+var App;
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
-var App = Ember.Application.extend({
-  modulePrefix: 'motorsports-stats-dot-com', // TODO: loaded via config
+App = Ember.Application.extend({
+  modulePrefix: config.modulePrefix,
+  podModulePrefix: config.podModulePrefix,
   Resolver: Resolver
 });
 
-loadInitializers(App, 'motorsports-stats-dot-com');
+loadInitializers(App, config.modulePrefix);
 
 export default App;
