b9b766b1131959dbdda301f0ba3e23ce4de8001d
The Game <chris.game@robbiebobbins.com>
2015-10-27 22:43:10 +0000

    better implementation of sorting

--- app/routes/f1.js
+++ app/routes/f1.js
@@ -3,8 +3,5 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(){
     return this.get('store').findAll('season');
-  },
-  afterModel: function(model){
-    model = model.reverseObjects();
   }
 });
