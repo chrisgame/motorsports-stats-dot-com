6c29b111f2f57e8239f242322f889a49c4e3ee99
The Game <chris.game@robbiebobbins.com>
2015-10-27 22:41:30 +0000

    updated to new finders

--- app/routes/f1.js
+++ app/routes/f1.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(){
-    return this.get('store').find('season');
+    return this.get('store').findAll('season');
   },
   afterModel: function(model){
     model = model.reverseObjects();
--- app/routes/season.js
+++ app/routes/season.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params){
-    return this.get('store').find('season', params.season_id);
+    return this.get('store').peekRecord('season', params.season_id);
   }
 });
