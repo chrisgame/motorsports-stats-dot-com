69880457afc6fb1f8af6b54c635191a9e6fd29f2
The Game <chris.game@robbiebobbins.com>
2015-10-27 22:42:19 +0000

    setting ember data defaults for clarity

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,4 +2,10 @@ import DS from 'ember-data';
 
 export default DS.ActiveModelAdapter.reopen({
   host: 'http://localhost:4200',
+  shouldReloadAll: function() {
+    return true;
+  },
+  shouldBackgroundReloadRecord: function() {
+    return true;
+  }
 });
