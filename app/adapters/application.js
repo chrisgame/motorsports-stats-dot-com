0db4f54f6a4cb9c6b2008f609a410a47584f3f30
The Game <chris.game@robbiebobbins.com>
2016-07-20 22:44:19 +0100

    restore active model adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,6 @@
-import DS from 'ember-data';
+import ActiveModelAdapter from 'active-model-adapter';
 
-export default DS.ActiveModelAdapter.reopen({
+export default ActiveModelAdapter.extend({
   shouldReloadAll: function() {
     return true;
   },
