81a1466741484b474a16a8e7982ed32b86d21044
The Game <chris.game@robbiebobbins.com>
2016-09-04 21:55:58 +0100

    mirage - new style scenario - broken

--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -1,4 +1,6 @@
 export default function(server) {
+  server.logging = true;
+
   const practiceBestSectors = server.createList('bestSector', 26);
   const practiceResults = server.createList('practiceResult', 26);
   let practiceSpeedTrapPositionId = 0;
@@ -20,44 +22,40 @@ export default function(server) {
   const pitStops = server.createList('pitStop', 26);
 
   const practiceSessions = server.createList('practiceSession', 1, {
-    bestSectorIds: practiceBestSectors.mapBy('id'),
-    practiceResultIds: practiceResults.mapBy('id'),
-    speedTrapIds: practiceSpeedTraps.mapBy('id')
+    practiceBestSectors,
+    practiceResults,
+    practiceSpeedTraps
   });
   const qualifySessions = server.createList('qualifySession', 1, {
-    bestSectorIds: qualifyBestSectors.mapBy('id'),
-    qualifyResultIds: qualifyResults.mapBy('id'),
-    speedTrapIds: qualifySpeedTraps.mapBy('id')
+    qualifyBestSectors,
+    qualifyResults,
+    qualifySpeedTraps
   });
 
   const practice = server.create('practice', {
-    practiceSessionIds: practiceSessions.mapBy('id')
+    practiceSessions
   });
   const qualify = server.create('qualify', {
-    qualifySessionIds: qualifySessions.mapBy('id')
+    qualifySessions
   });
   const race = server.create('race', {
-    fastestLapIds: fastestLaps.mapBy('id'),
-    pitStopIds: pitStops.mapBy('id'),
-    raceResultIds: raceResults.mapBy('id')
+    fastestLaps,
+    pitStops,
+    raceResults
   });
 
-  server.logging = true;
+  const rounds = server.createList('round', 21, {
+    practiceId,
+    qualifyId,
+    raceId
+  });
+
   //62
   //67
-  server.createList('season', 1, {
+  server.createList('season', 67, {
     id: (i) => {
       return 2016 - i;
     },
-    //21
-    round_ids: () => {
-      const rounds = server.createList('round', 1, {
-        practiceId: practice.id,
-        qualifyId: qualify.id,
-        raceId: race.id
-      });
-
-      return rounds.mapBy('id');
-    }
+    rounds
   });
 }
