a91a75026f80a2c643972006082bdeb0e6cb0502
The Game <chris.game@robbiebobbins.com>
2017-03-03 21:53:29 +0000

    get custom transform working

--- app/transforms/duration.js
+++ app/transforms/duration.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 import moment from 'moment';
 
 export default DS.Transform.extend({
-  deserialize: function(serialized) {
+  deserialize(serialized) {
     var duration = moment.duration(parseFloat(serialized), 'seconds');
     return serialized ? duration.minutes() + ':' + duration.seconds() + '.' + duration.milliseconds() : null;
   }
