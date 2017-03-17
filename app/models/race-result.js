8a72a7186ee1cb12a8503b9b5baa64aea3e0d21a
The Game <chris.game@robbiebobbins.com>
2017-03-17 23:07:41 +0000

    made race-results sortable

--- app/models/race-result.js
+++ app/models/race-result.js
@@ -2,12 +2,12 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   round: DS.belongsTo('round'),
-  position: DS.attr('string'),
+  position: DS.attr('number'),
   carNumber: DS.attr('number'),
   driver: DS.attr('string'),
   team: DS.attr('string'),
   laps: DS.attr('number'),
   timeOrRetirement: DS.attr('string'),
-  grid: DS.attr('string'),
-  points: DS.attr('string')
+  grid: DS.attr('number'),
+  points: DS.attr('number')
 });
